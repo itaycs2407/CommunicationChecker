53d23ea9534fa446ef9ff48f182a13a9691f4987
Itay Cohen <itaycs2407@gmail.com>
2020-12-06 21:52:16 +0200

    added example for compile via process

---
+++ compileViaProcess.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo(@"cmd");
+            psi.UseShellExecute = false;
+            psi.CreateNoWindow = false;
+            psi.RedirectStandardInput = true;
+            var proc = Process.Start(psi);
+
+            StreamWriter writer = proc.StandardInput;
+            writer.WriteLine(@"cd C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\Tools");
+            writer.WriteLine("VsDevCmd.bat");
+            writer.WriteLine(@"cl C:\Users\itayco\Desktop\ITAY\client.cpp -o C:\Users\itayco\Desktop\ITAY\momo.exe");
+            Console.ReadLine();
+            // Process p = new Process("cmd",);
+
+        }
+    }
+}
