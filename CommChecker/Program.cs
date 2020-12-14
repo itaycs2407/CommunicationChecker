4e0ceb274315eeb02eba69941a3efb685165ca2a
Itay Cohen <itaycs2407@gmail.com>
2020-12-14 19:02:57 +0200

    added main menu - need to finish

--- CommChecker/Program.cs
+++ CommChecker/Program.cs
@@ -10,6 +10,7 @@ namespace CommChecker
     {
         static void Main(string[] args)
         {
+            /*
             Console.WriteLine("Enter working Directory : ");
             string workingDirectory = Console.ReadLine();
             Console.WriteLine("Enter comments filename : ");
@@ -18,6 +19,52 @@ namespace CommChecker
             cc.Run();
             Console.WriteLine("Program finished. Press any key....");
             Console.ReadLine();
+            */
+            GetInputMainMenu();
+        }
+
+        public static void GetInputMainMenu()
+        {
+            printMenu();
+
+            string inputStr = Console.ReadLine(); ;
+            int input;
+            while (!(int.TryParse(inputStr, out input) && checkLimit(input, 1, 4)))
+            {
+                printMenu();
+                inputStr = Console.ReadLine();
+            }/*
+            switch (input)
+            {
+                case 1: extractAllFiles();
+                    break;
+                case 2: compileFiles();
+                    break;
+                case 3: copyComments();
+                    break;
+                case 4: Environment.Exit(0);
+                    break;
+                                
+            }*/
+
+
+
+        }
+
+        private static bool checkLimit(int i_Value, int i_Low, int i_High)
+        {
+            return i_Value >= i_Low && i_Value <= i_High;
+        }
+
+        private static void printMenu()
+        {
+            Console.WriteLine("MainMenu");
+            Console.WriteLine("******************");
+            Console.WriteLine("1. extract all files.");
+            Console.WriteLine("2. compile (cpp) files using cl compiler.");
+            Console.WriteLine("3. copy comments file.");
+            Console.WriteLine("4. exit.");
+            Console.WriteLine("Enter choise : ");
         }
     }
 }
